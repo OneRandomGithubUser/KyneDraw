1ae39a7200984ce89d0006192dd11e71705da996
OneRandomGithubUser <60021548+OneRandomGithubUser@users.noreply.github.com>
2022-02-05 11:27:31 -0500

    change to webgl

--- orgo.js
+++ orgo.js
@@ -33,8 +33,8 @@ function setup() {
   // createCanvas must be the first statement
   windowWidth = Math.max(window.innerWidth,minWidth);
   windowHeight = Math.max(window.innerHeight,minHeight);
-  createCanvas(windowWidth,windowHeight);
-  buffer = createGraphics(windowWidth,windowHeight);
+  createCanvas(windowWidth,windowHeight,WEBGL);
+  buffer = createGraphics(windowWidth,windowHeight,WEBGL);
   buffer.stroke(0); // Set line drawing color to black
   frameRate(60);
   console.log("Written by Joseph. github.com/OneRandomGithubUser");
@@ -42,7 +42,7 @@ function setup() {
 
 let font;
 function preload() {
-  font = loadFont('./arial.ttf');
+  font = loadFont('arial');
 }
 
 function draw() {
