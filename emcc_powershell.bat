3b9986a009f5f55cd0acc339d72009b185347399
OneRandomGithubUser <60021548+OneRandomGithubUser@users.noreply.github.com>
2022-05-06 10:07:09 -0400

    update emcc bat files
    update emcc bat files to use c++17 and -lembind as they should

--- emcc_powershell.bat
+++ emcc_powershell.bat
@@ -1,6 +1,6 @@
-powershell -NoExit cd path\to\emcc; Add-Content -path (Get-PSReadlineOption).HistorySavePath 'emcc path\to\cpp\file -o path\of\output\js\file -s USE_BOOST_HEADERS=1'; emsdk_env.bat
+powershell -NoExit cd path\to\emcc; Add-Content -path (Get-PSReadlineOption).HistorySavePath 'emcc path\to\cpp\file -o path\of\output\js\file -s USE_BOOST_HEADERS=1 -std=c++17 -lembind'; emsdk_env.bat
 :: powershell -NoExit opens powershell without exiting after the command is complete
 :: cd path\to\emcc opens the path to the emcc, not necessary if emsdk is activated latest is run with the --permanent flag (emsdk activate latest --permanent)
 :: ; strings multiple powershell commands together
-:: Add-Content -path (Get-PSReadlineOption).HistorySavePath 'emcc path\to\cpp\file -o path\of\output\js\file -s USE_BOOST_HEADERS=1' adds the command to be run to the console history so that it can be run more easily
+:: Add-Content -path (Get-PSReadlineOption).HistorySavePath 'emcc path\to\cpp\file -o path\of\output\js\file -s USE_BOOST_HEADERS=1 -std=c++17 -lembind' adds the command to be run to the console history so that it can be run more easily
 :: emsdk_env.bat enters the emsdk environment
--- emcc_windows_terminal.bat
+++ emcc_windows_terminal.bat
@@ -1,6 +1,6 @@
-wt -d path\to\emcc powershell -NoExit Add-Content -path (Get-PSReadlineOption).HistorySavePath 'emcc path\to\cpp\file -o path\of\output\js\file -s USE_BOOST_HEADERS=1' \; .\emsdk_env.bat
+wt -d path\to\emcc powershell -NoExit Add-Content -path (Get-PSReadlineOption).HistorySavePath 'emcc path\to\cpp\file -o path\of\output\js\file -s USE_BOOST_HEADERS=1 -std=c++17 -lembind' \; .\emsdk_env.bat
 :: wt -d path\to\emcc opens Windows Terminal at the directory of emcc, "-d path\to\emcc" is not necessary if emsdk is activated latest is run with the --permanent flag (emsdk activate latest --permanent)
 :: powershell -NoExit opens Powershell within Windows Terminal without exiting after the command is complete 
-:: Add-Content -path (Get-PSReadlineOption).HistorySavePath 'emcc path\to\cpp\file -o path\of\output\js\file -s USE_BOOST_HEADERS=1' adds the command to be run to the console history so that it can be run more easily
+:: Add-Content -path (Get-PSReadlineOption).HistorySavePath 'emcc path\to\cpp\file -o path\of\output\js\file -s USE_BOOST_HEADERS=1 -std=c++17 -lembind' adds the command to be run to the console history so that it can be run more easily
 :: \; strings multiple powershell commands together, properly escaped so that Windows Terminal doesn't run them in separate windows
 :: emsdk_env.bat enters the emsdk environment
