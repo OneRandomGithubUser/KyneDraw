9b2702c9e7147401eda184b95767ec3fbccd7419
OneRandomGithubUser <60021548+OneRandomGithubUser@users.noreply.github.com>
2022-05-11 17:15:11 -0400

    consolidate canvas interaction methods
    consolidate canvas interaction methods to take advantage of static variables

--- main.cpp
+++ main.cpp
@@ -19,9 +19,11 @@
 namespace kynedraw
 {
   class Molecule;
+  class VisibleMolecule;
   class Bond;
+  class VisibleBond;
   class Node;
-  class Atom;
+  class VisibleNode;
   class Molecule {
     //
   };
@@ -29,9 +31,9 @@ namespace kynedraw
   {
     //
   };
-  class Atom
+  class Node
   {
-    protected:
+  protected:
     std::string name;
     int numBonds;
     int charge;
@@ -40,22 +42,20 @@ namespace kynedraw
     std::weak_ptr<kynedraw::Molecule> molecule;
     std::unordered_map<boost::uuids::uuid, std::weak_ptr<kynedraw::Bond>, boost::hash<boost::uuids::uuid>> bondList;
     std::unordered_map<boost::uuids::uuid, std::weak_ptr<kynedraw::Node>, boost::hash<boost::uuids::uuid>> nodeList;
-
-    public:
-    Atom(std::string name)
+  public:
+    Node(std::string name)
     {
       this->name = name;
     }
   };
-  class Node : protected Atom
+  class VisibleNode : protected Node
   {
-    protected:
+  protected:
     double x;
     double y;
     double predictedNextBondAngleList[3];
-
-    public:
-    Node(std::string name, double x, double y) : Atom(name)
+  public:
+    VisibleNode(std::string name, double x, double y) : Node(name)
     {
       this->x = x;
       this->y = y;
@@ -85,85 +85,20 @@ namespace kynedraw
       this->y = y;
     }
   };
+  class Network
+  {
+  protected:
+    std::unordered_map<boost::uuids::uuid, kynedraw::VisibleNode, boost::hash <boost::uuids::uuid>> visibleNetwork;
+    std::unordered_map<boost::uuids::uuid, kynedraw::Node, boost::hash <boost::uuids::uuid>> network;
+    std::unordered_map<boost::uuids::uuid, kynedraw::VisibleBond, boost::hash <boost::uuids::uuid>> visibleBonds;
+    std::unordered_map<boost::uuids::uuid, kynedraw::Bond, boost::hash <boost::uuids::uuid>> bonds;
+  };
+  class Preview : protected Network
+  {
+  protected:
+    std::weak_ptr<kynedraw::VisibleNode> pivotNode;
+  };
 }
-
-const double pi = std::numbers::pi;
-int frameCount = 0;
-emscripten::val window = emscripten::val::global("window");
-emscripten::val document = emscripten::val::global("document");
-emscripten::val localStorage = emscripten::val::global("localStorage");
-boost::uuids::random_generator uuidGenerator;
-std::string selectedTool;
-std::string bondSnapSetting;
-std::unordered_map<boost::uuids::uuid, kynedraw::Node, boost::hash < boost::uuids::uuid>> visibleNetwork;
-std::unordered_map<boost::uuids::uuid, kynedraw::Atom, boost::hash < boost::uuids::uuid>> network;
-std::unordered_map<boost::uuids::uuid, kynedraw::Node, boost::hash < boost::uuids::uuid>> visiblePreview;
-std::unordered_map<boost::uuids::uuid, kynedraw::Atom, boost::hash < boost::uuids::uuid>> preview;
-const std::unordered_map<std::string, int> buttonIDs = {
-  {"bonds", -1},
-  {"single", 10},
-  {"double", 11},
-  {"triple", 12},
-  {"atoms", -2},
-  {"hydrogen", 20},
-  {"carbon", 21},
-  {"oxygen", 22},
-  {"nitrogen", 23},
-  {"bromine", 24},
-  {"chlorine", 25},
-  {"sulfur", 26},
-  {"drag-delete-tools", -3},
-  {"atom-bond-drag", 30},
-  {"molecule-drag", 31},
-  {"atom-bond-delete", 32},
-  {"molecule-delete", 33},
-  {"charge-h-tools", -4},
-  {"add-charge", 40},
-  {"subtract-charge", 41},
-  {"add-h", 42},
-  {"subtract-h", 43},
-  {"snap", 200},
-  {"freeform", 201},
-  {"clear", 202},
-  {"random", 203},
-  {"hackerman", 204},
-  {"sn-e", -10},
-  {"alkene-addition", -11},
-  {"ring-metathesis", 111},
-  {"hbr-addition", 112},
-  {"radical-hbr", 113},
-  {"halogenation", 114},
-  {"halohydrin-formation", 115},
-  {"hydrogenation", 116},
-  {"cis-hydrogenation", 117},
-  {"trans-hydrogenation", 118},
-  {"cyclopropanation", 119},
-  {"alkene-oxidation", -12},
-  {"acid-hydration", 120},
-  {"oxymercuration", 121},
-  {"hydroboration", 122},
-  {"alkyne-hydration", 123},
-  {"alkyne-hydroboration", 124},
-  {"epoxidation", 125},
-  {"dihydroxylation", 126},
-  {"wacker-oxidation", 127},
-  {"ozonolysis", 128},
-  {"oxidative-cleavage", 129},
-  {"alcohol-reactions", -13},
-  {"gringard-formation", 130},
-  {"organolithiate-formation", 131},
-  {"organocopper-formation", 132},
-  {"dehydration", 133},
-  {"weak-reduction", 134},
-  {"strong-reduction", 135},
-  {"weak-oxidation", 136},
-  {"jones-oxidation", 137},
-  {"bromination", 138},
-  {"chlorination", 139},
-  {"tosylation", 140},
-  {"hydroxyl-protection", 141},
-  {"hydroxyl-unprotection", 142}
-};
 
 /*{
   {"bonds", -1},
@@ -230,6 +165,87 @@ const std::unordered_map<std::string, int> buttonIDs = {
   {"hydroxyl-unprotection", 52}
 };*/
 
+const std::unordered_map<std::string, int> buttonIDs = {
+    {"bonds", -1},
+    {"single", 10},
+    {"double", 11},
+    {"triple", 12},
+    {"atoms", -2},
+    {"hydrogen", 20},
+    {"carbon", 21},
+    {"oxygen", 22},
+    {"nitrogen", 23},
+    {"bromine", 24},
+    {"chlorine", 25},
+    {"sulfur", 26},
+    {"drag-delete-tools", -3},
+    {"atom-bond-drag", 30},
+    {"molecule-drag", 31},
+    {"atom-bond-delete", 32},
+    {"molecule-delete", 33},
+    {"charge-h-tools", -4},
+    {"add-charge", 40},
+    {"subtract-charge", 41},
+    {"add-h", 42},
+    {"subtract-h", 43},
+    {"snap", 200},
+    {"freeform", 201},
+    {"clear", 202},
+    {"random", 203},
+    {"hackerman", 204},
+    {"sn-e", -10},
+    {"alkene-addition", -11},
+    {"ring-metathesis", 111},
+    {"hbr-addition", 112},
+    {"radical-hbr", 113},
+    {"halogenation", 114},
+    {"halohydrin-formation", 115},
+    {"hydrogenation", 116},
+    {"cis-hydrogenation", 117},
+    {"trans-hydrogenation", 118},
+    {"cyclopropanation", 119},
+    {"alkene-oxidation", -12},
+    {"acid-hydration", 120},
+    {"oxymercuration", 121},
+    {"hydroboration", 122},
+    {"alkyne-hydration", 123},
+    {"alkyne-hydroboration", 124},
+    {"epoxidation", 125},
+    {"dihydroxylation", 126},
+    {"wacker-oxidation", 127},
+    {"ozonolysis", 128},
+    {"oxidative-cleavage", 129},
+    {"alcohol-reactions", -13},
+    {"gringard-formation", 130},
+    {"organolithiate-formation", 131},
+    {"organocopper-formation", 132},
+    {"dehydration", 133},
+    {"weak-reduction", 134},
+    {"strong-reduction", 135},
+    {"weak-oxidation", 136},
+    {"jones-oxidation", 137},
+    {"bromination", 138},
+    {"chlorination", 139},
+    {"tosylation", 140},
+    {"hydroxyl-protection", 141},
+    {"hydroxyl-unprotection", 142}
+};
+const double pi = std::numbers::pi;
+int FRAME_COUNT = 0;
+double DEADZONE_WIDTH = 5;
+double DEADZONE_HEIGHT = 5;
+double BOND_LENGTH = 50;
+emscripten::val window = emscripten::val::global("window");
+emscripten::val document = emscripten::val::global("document");
+emscripten::val localStorage = emscripten::val::global("localStorage");
+boost::uuids::random_generator uuidGenerator;
+std::string selectedTool;
+std::string bondSnapSetting;
+std::unordered_map<boost::uuids::uuid, kynedraw::VisibleNode, boost::hash <boost::uuids::uuid>> visibleNetwork;
+std::unordered_map<boost::uuids::uuid, kynedraw::Node, boost::hash <boost::uuids::uuid>> network;
+std::unordered_map<boost::uuids::uuid, kynedraw::VisibleNode, boost::hash <boost::uuids::uuid>> visiblePreview;
+std::unordered_map<boost::uuids::uuid, kynedraw::Node, boost::hash <boost::uuids::uuid>> preview;
+
 void render()
 {
   // NOTE: if this is not defined in the function, embind throws an error at runtime
@@ -239,12 +255,12 @@ void render()
   ctx.call<void>("clearRect", 0, 0, canvas["width"], canvas["height"]);
   ctx.set("fillStyle", "green");
   ctx.call<void>("beginPath");
-  ctx.call<void>("rect", 100 * sin(frameCount/360.0), 100 * cos(frameCount/360.0), 150, 100);
+  ctx.call<void>("rect", 100 * sin(FRAME_COUNT / 360.0), 100 * cos(FRAME_COUNT / 360.0), 150, 100);
   ctx.call<void>("fill");
   ctx.call<void>("stroke");
   ctx.set("fillStyle", "red");
   ctx.call<void>("beginPath");
-  ctx.call<void>("arc", 100 * sin(frameCount/360.0), 100 * cos(frameCount/360.0), 5, 0, 2 * pi);
+  ctx.call<void>("arc", 100 * sin(FRAME_COUNT / 360.0), 100 * cos(FRAME_COUNT / 360.0), 5, 0, 2 * pi);
   ctx.call<void>("stroke");
   for (const auto& [uuid, currentVisibleNode] : visibleNetwork)
   {
@@ -258,33 +274,13 @@ void render()
     ctx.call<void>("arc", currentVisiblePreviewNode.get_x(), currentVisiblePreviewNode.get_y(), 5, 0, 2 * pi);
     ctx.call<void>("stroke");
   }
-  frameCount++;
-}
-
-void Click(emscripten::val event)
-{
-  static double mouseDownPageX, mouseDownPageY;
-  std::string eventName = event["type"].as<std::string>();
-  std::cout << eventName << "\n";
-  if (eventName == "mousedown") {
-    // TODO: make a dead zone so slightly dragging the mouse doesn't count as a drag, and also find out how to concatenate two maps
-    kynedraw::Atom node("C");
-    network.try_emplace(uuidGenerator(), node);
-    kynedraw::Node visibleNode("C", event["pageX"].as<double>(), event["pageY"].as<double>());
-    visibleNetwork.try_emplace(uuidGenerator(), visibleNode);
-  }
+  FRAME_COUNT++;
 }
 
 void AddButtonEventListeners(emscripten::val element, emscripten::val index, emscripten::val array)
 {
   element.call<void>("addEventListener", emscripten::val("mousedown"), emscripten::val::module_property("ClickButton"));
   element.call<void>("addEventListener", emscripten::val("mouseup"), emscripten::val::module_property("ClickButton"));
-}
-
-void ClickButton(emscripten::val event)
-{
-  int clickedButtonID = buttonIDs.at(event["target"]["id"].as<std::string>());
-  event.call<void>("stopPropagation");
 }
 
 void AddToolButtonEventListener(emscripten::val element, emscripten::val index, emscripten::val array)
@@ -307,18 +303,23 @@ void StoreBondSnapSetting(emscripten::val event)
   localStorage.call<void>("setItem", emscripten::val("bondSnapSetting"), event["target"]["id"]);
 }
 
-void UpdateSelectedTool(std::string tool)
+void ResetPreview(std::string tool, double pageX, double pageY)
 {
   int toolID = buttonIDs.at(tool);
   preview.clear();
   visiblePreview.clear();
   switch (buttonIDs.at(tool)) {
+    case 10:
+      // single
+      preview.try_emplace(uuidGenerator(), kynedraw::Node("C"));
+      visiblePreview.try_emplace(uuidGenerator(), kynedraw::VisibleNode("C", pageX, pageY));
+      preview.try_emplace(uuidGenerator(), kynedraw::Node("C"));
+      visiblePreview.try_emplace(uuidGenerator(), kynedraw::VisibleNode("C", pageX+BOND_LENGTH*std::cos(pi*11/6), pageY+BOND_LENGTH*std::sin(pi*11/6)));
+      break;
     case 21:
       // carbon
-      kynedraw::Atom node("C");
-      preview.try_emplace(uuidGenerator(), node);
-      kynedraw::Node visibleNode("C", -1.0, -1.0);
-      visiblePreview.try_emplace(uuidGenerator(), visibleNode);
+      preview.try_emplace(uuidGenerator(), kynedraw::Node("C"));
+      visiblePreview.try_emplace(uuidGenerator(), kynedraw::VisibleNode("C", pageX, pageY));
       break;
   }
 }
@@ -346,23 +347,57 @@ void InitializeAllSettings()
   selectedTool = RetrieveAndTickSetting("selectedTool", "single");
   bondSnapSetting = RetrieveAndTickSetting("bondSnapSetting", "freeform");
 
-  // initialize the selectedTool
-  UpdateSelectedTool(selectedTool);
+  // initialize the selectedTool at (0,0) because that is what static double previousPageX, previousPageY in MouseMove is initialized to
+  ResetPreview(selectedTool, 0, 0);
 }
 
-void MovePreview(emscripten::val event)
+void ClickButton(emscripten::val event)
 {
-  static double previousPageX;
-  static double previousPageY;
-  double pageX = event["pageX"].as<double>();
-  double pageY = event["pageY"].as<double>();
-  for (auto& [uuid, currentVisiblePreviewNode] : visiblePreview)
+  selectedTool = event["target"]["id"].as<std::string>();
+  ResetPreview(selectedTool, event["pageX"].as<double>(), event["pageY"].as<double>());
+  event.call<void>("stopPropagation");
+}
+
+void MouseMove(double pageX, double pageY, bool mouseIsPressed, double mouseDownPageX, double mouseDownPageY)
+{
+  static double previousPageX, previousPageY;
+  if (mouseIsPressed && std::abs(mouseDownPageX - pageX) < DEADZONE_WIDTH && std::abs(mouseDownPageY - pageY) < DEADZONE_HEIGHT)
   {
-    currentVisiblePreviewNode.change_x(pageX - previousPageX);
-    currentVisiblePreviewNode.change_y(pageY - previousPageY);
+    //
+  } else {
+    for (auto&[uuid, currentVisiblePreviewNode]: visiblePreview) {
+      currentVisiblePreviewNode.change_x(pageX - previousPageX);
+      currentVisiblePreviewNode.change_y(pageY - previousPageY);
+    }
   }
   previousPageX = pageX;
   previousPageY = pageY;
+}
+
+void InteractWithCanvas(emscripten::val event)
+{
+  static double mouseDownPageX, mouseDownPageY;
+  static bool mouseIsPressed;
+
+  std::string eventName = event["type"].as<std::string>();
+  double pageX = event["pageX"].as<double>();
+  double pageY = event["pageY"].as<double>();
+
+  // NOTE: mousemove fires before mousedown and mouseup on an animation frame, at least in Chrome
+  if (eventName == "mousemove") {
+    MouseMove(pageX, pageY, mouseIsPressed, mouseDownPageX, mouseDownPageY);
+  } else if (eventName == "mousedown") {
+    mouseDownPageX = pageX;
+    mouseDownPageY = pageY;
+    mouseIsPressed = true;
+  } else if (eventName == "mouseup") {
+    // TODO: make a dead zone so slightly dragging the mouse doesn't count as a drag, and also find out how to concatenate two maps
+    // If mouseIsPressed is false and there is a mouseup event, that means that the mouse was clicked and dragged from a UI button to the canvas
+    network.merge(preview);
+    visibleNetwork.merge(visiblePreview);
+    mouseIsPressed = false;
+    ResetPreview(selectedTool, pageX, pageY);
+  }
 }
 
 void ResizeCanvas(emscripten::val event)
@@ -380,9 +415,9 @@ void RunMainLoop()
 int main()
 {
   window.call<void>("addEventListener", emscripten::val("resize"), emscripten::val::module_property("ResizeCanvas"));
-  document.call<void>("addEventListener", emscripten::val("mousedown"), emscripten::val::module_property("Click"));
-  document.call<void>("addEventListener", emscripten::val("mouseup"), emscripten::val::module_property("Click"));
-  document.call<void>("addEventListener", emscripten::val("mousemove"), emscripten::val::module_property("MovePreview"));
+  document.call<void>("addEventListener", emscripten::val("mousedown"), emscripten::val::module_property("InteractWithCanvas"));
+  document.call<void>("addEventListener", emscripten::val("mouseup"), emscripten::val::module_property("InteractWithCanvas"));
+  document.call<void>("addEventListener", emscripten::val("mousemove"), emscripten::val::module_property("InteractWithCanvas"));
 
   // add event listeners, which search for button elements or for labels that are at the same level in the DOM tree as the radio buttons with the specified name
   // NOTE: this will affect EVERY <button> element. Not a problem for now...
@@ -404,8 +439,7 @@ int main()
 
 EMSCRIPTEN_BINDINGS(bindings)
 {
-  emscripten::function("Click", Click);
-  emscripten::function("MovePreview", MovePreview);
+  emscripten::function("InteractWithCanvas", InteractWithCanvas);
   emscripten::function("ResizeCanvas", ResizeCanvas);
   emscripten::function("ClickButton", ClickButton);
   emscripten::function("AddButtonEventListeners", AddButtonEventListeners);
